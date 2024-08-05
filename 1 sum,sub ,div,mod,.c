81543772f3b4adfcb1f187de4a2d05b75ea0b93e
kresha116 <kresha502@gmail.com>
2024-08-05 19:18:43 +0530

    Update

---
+++ 1 sum,sub ,div,mod,.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	    int n1=12,n2=6;
+	    printf("sum %d and %d is %d\n",n1,n2,n1+n2);
+	    printf("sub %d and %d is %d\n",n1,n2,n1-n2);
+	    printf("mul %d and %d is %d\n",n1,n2,n1*n2);
+	    printf("div %d and %d is %d\n",n1,n2,n1/n2);
+	    printf("mod %d and %d is %d\n",n1,n2,n1%n2);
+	    
+	    printf("%d + %d = %d\n",n1,n2,n1+n2);
+	    printf("%d - %d = %d\n",n1,n2,n1-n2);
+	    printf("%d * %d = %d\n",n1,n2,n1*n2);
+	    printf("%d / %d = %d\n",n1,n2,n1%n2);
+	    
+
+}
+
---
+++ 2 hello and my information.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+	printf("hello world\n");
+	printf("name\t: my name is kresha\n ");
+	printf("age\t:i am 17 year old\n");
+	printf("school\t:my school name is aashadeep 4\n");
+
+}
